4674eb77fb3f2b1e7ffc3c0b0de359be24bd37c8
superbeom <it.super.beom@gmail.com>
2022-06-29 09:17:02 +0900

    Test ChannelTalk 3

--- pages/index.js
+++ pages/index.js
@@ -64,58 +64,6 @@ export default function Home() {
             </p>
           </a>
         </div>
-
-        <script>
-          {(function () {
-            var w = window;
-
-            if (w.ChannelIO) {
-              return (
-                window.console.error ||
-                window.console.log ||
-                function () {}
-              )("ChannelIO script included twice.");
-            }
-
-            var ch = function () {
-              ch.c(arguments);
-            };
-
-            ch.q = [];
-            ch.c = function (args) {
-              ch.q.push(args);
-            };
-
-            w.ChannelIO = ch;
-
-            function l() {
-              if (w.ChannelIOInitialized) {
-                return;
-              }
-
-              w.ChannelIOInitialized = true;
-              var s = document.createElement("script");
-              s.type = "text/javascript";
-              s.async = true;
-              s.src = "https://cdn.channel.io/plugin/ch-plugin-web.js";
-              s.charset = "UTF-8";
-              var x = document.getElementsByTagName("script")[0];
-              x.parentNode.insertBefore(s, x);
-            }
-
-            if (document.readyState === "complete") {
-              l();
-            } else if (window.attachEvent) {
-              window.attachEvent("onload", l);
-            } else {
-              window.addEventListener("DOMContentLoaded", l, false);
-              window.addEventListener("load", l, false);
-            }
-          })()}
-          {ChannelIO("boot", {
-            pluginKey: "318cc6ca-5597-4f84-94ec-775a7ba55153",
-          })}
-        </script>
       </main>
 
       <footer className={styles.footer}>
