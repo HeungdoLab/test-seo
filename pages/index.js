a2e11e7489122cc4159151f7fa0315e7486e067f
superbeom <it.super.beom@gmail.com>
2022-07-28 09:58:24 +0900

    Add console log

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import Page from "../components/Page";
 
 export default function Home() {
+  console.log("This is Home page");
   return (
     <Page>
-      <h1>This is the Home page</h1>
+      <h1>This is Home page</h1>
     </Page>
   );
 }
