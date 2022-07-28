686bd2321b2d0ba9bb2b68d448fa545b63a34101
superbeom <it.super.beom@gmail.com>
2022-07-28 10:12:11 +0900

    Test remove console 2

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: ["transform-remove-console"],
-  },
 };
 
 module.exports = nextConfig;
